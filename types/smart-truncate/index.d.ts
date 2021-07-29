931c4047c0fe7f467ea20396fab25c5bcf703440
Даниил Пронин <mail@grawl.ru>
2021-07-29 17:21:14 +1000

    fix smart-truncate

--- types/smart-truncate/index.d.ts
+++ types/smart-truncate/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for smart-truncate 1.0
+// Type definitions for smart-truncate 1.0.1
 // Project: https://github.com/millerized/smart-truncate
 // Definitions by: Omer Yalhi <https://github.com/oyalhi>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-export function smartTruncate(text: string, length: number, options?: { position?: number | undefined; mark?: string | undefined }): string;
+export default function smartTruncate(text: string, length: number, options?: { position?: number | undefined; mark?: string | undefined }): string;
